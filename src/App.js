18442f445315d027e83ad03831ad79b9f7f8c046
ishitaanand2222 <ishitaanand2222@gmail.com>
2023-07-01 20:20:24 +0530

    Adding delete handler

--- src/App.js
+++ src/App.js
@@ -37,6 +37,13 @@ class App extends Component{
       ]}
     )
   }
+
+  deletePersonHandler = (personIndex) =>{
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
   render(){
     const style = {
       backgroundColor: 'white',
@@ -52,13 +59,14 @@ class App extends Component{
       //   return <Person name={person.name} age={person.age}/>
       // })
       persons = (
-        <>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
-          click = {this.switchNameHandler.bind(this, "Jonny !")}
-          changed = {this.nameChangedHandler}> My Hobbies: Racing </Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-        </>
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+            click={() => this.deletePersonHandler(index)} 
+            name={person.name} 
+            age={person.age}/>
+          })}
+        </div>
       )
     }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Person.css';
 
 const Person = (props) => {
     console.log("hey"+props.children);
