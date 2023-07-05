671a4c1de957f88a4f2783bdeae2373604c3b7d1
ishitaanand2222 <ishitaanand2222@gmail.com>
2023-07-05 16:29:58 +0530

    Way of wrapping elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,13 +4,11 @@ import classes from './Person.module.css';
 class Person extends Component{
     render(){
         console.log("[Person.js] child prop rendering")
-        return (
-            <div className={classes.Person} > 
-                <p onClick={this.props.click}>Hi my name is {this.props.name} and age is {this.props.age}</p>
-                <p>{this.props.children}</p>
+        return [ 
+                <p onClick={this.props.click}>Hi my name is {this.props.name} and age is {this.props.age}</p>,
+                <p>{this.props.children}</p>,
                 <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-            </div>
-        )
+        ]
     }
 }
 
