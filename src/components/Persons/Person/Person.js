30c9678e7c0220d582d56bc744849ef498ec3c1e
ishitaanand2222 <ishitaanand2222@gmail.com>
2023-07-05 16:33:29 +0530

    Adding key to the return array

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,9 +5,9 @@ class Person extends Component{
     render(){
         console.log("[Person.js] child prop rendering")
         return [ 
-                <p onClick={this.props.click}>Hi my name is {this.props.name} and age is {this.props.age}</p>,
-                <p>{this.props.children}</p>,
-                <input type="text" onChange={this.props.changed} value={this.props.name}></input>
+                <p key="i1" onClick={this.props.click}>Hi my name is {this.props.name} and age is {this.props.age}</p>,
+                <p key="i2">{this.props.children}</p>,
+                <input key = "i3" type="text" onChange={this.props.changed} value={this.props.name}></input>
         ]
     }
 }
