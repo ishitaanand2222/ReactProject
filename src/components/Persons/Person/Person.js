1e2079ceb9f361ffc121a859678693f2ad439a67
ishitaanand2222 <ishitaanand2222@gmail.com>
2023-07-05 16:44:23 +0530

    Aux for wrapping jsx

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,17 @@
 import React, {Component} from "react";
 import classes from './Person.module.css';
+import Auxillary from '../../../hoc/Auxillary';
 
 class Person extends Component{
     render(){
         console.log("[Person.js] child prop rendering")
-        return [ 
-                <p key="i1" onClick={this.props.click}>Hi my name is {this.props.name} and age is {this.props.age}</p>,
-                <p key="i2">{this.props.children}</p>,
+        return(
+            <Auxillary>
+                <p key="i1" onClick={this.props.click}>Hi my name is {this.props.name} and age is {this.props.age}</p>
+                <p key="i2">{this.props.children}</p>
                 <input key = "i3" type="text" onChange={this.props.changed} value={this.props.name}></input>
-        ]
+            </Auxillary>
+        ) 
     }
 }
 
